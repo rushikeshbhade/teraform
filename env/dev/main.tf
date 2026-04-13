de3417c95d4ae85208d902089d119aa1df66d675
rushikeshbhade <bhade.rushikesh12@gmail.com>
2026-04-13 13:11:01 +0530

    deploy

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -12,12 +12,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.20"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.7.2"
     }
   }
 }
