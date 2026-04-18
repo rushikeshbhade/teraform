7e51f188c0832c4fcfd8b29da8072b4080a16151
rushikeshbhade <bhade.rushikesh12@gmail.com>
2026-04-18 10:22:58 +0530

    changes

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -2,19 +2,19 @@ terraform {
   required_version = "~> 1.3.0"
 
   backend "s3" {
-    bucket       = "aws-304188066464-us-east-2-terraform-remote-state"
-    key          = "dev/terraform.tfstate"
-    region       = "us-east-2"
-    encrypt      = true
+    bucket  = "aws-304188066464-us-east-2-terraform-remote-state"
+    key     = "dev/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
   }
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
 
     random = {
-      source  = "hashicorp/random"
+      source = "hashicorp/random"
     }
   }
 }
